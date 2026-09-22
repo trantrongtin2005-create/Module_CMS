842c02d1cb78de8ff172cb3e2c61846a48b2007f
NguyenThiNhii <nhi7158456@gmail.com>
2026-09-23 01:23:06 +0700

    commit module8

--- wordpress/wp-content/themes/twentytwentyone/single.php
+++ wordpress/wp-content/themes/twentytwentyone/single.php
@@ -17,6 +17,9 @@ while ( have_posts() ) :
 
 	get_template_part( 'template-parts/content/content-single' );
 
+	// 7. Prev - Next Post (nằm ngay dưới Detail (6))
+	get_template_part( 'template-parts/post/navigation' );
+
 	if ( is_attachment() ) {
 		// Parent post navigation.
 		the_post_navigation(
@@ -27,24 +30,10 @@ while ( have_posts() ) :
 		);
 	}
 
-	// If comments are open or there is at least one comment, load up the comment template.
+	// 8. Comments (nằm ngay dưới Prev - Next Post (7))
 	if ( comments_open() || get_comments_number() ) {
 		comments_template();
 	}
-
-	// Previous/next post navigation.
-	$twentytwentyone_next = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' );
-	$twentytwentyone_prev = is_rtl() ? twenty_twenty_one_get_icon_svg( 'ui', 'arrow_right' ) : twenty_twenty_one_get_icon_svg( 'ui', 'arrow_left' );
-
-	$twentytwentyone_next_label     = esc_html__( 'Next post', 'twentytwentyone' );
-	$twentytwentyone_previous_label = esc_html__( 'Previous post', 'twentytwentyone' );
-
-	the_post_navigation(
-		array(
-			'next_text' => '<p class="meta-nav">' . $twentytwentyone_next_label . $twentytwentyone_next . '</p><p class="post-title">%title</p>',
-			'prev_text' => '<p class="meta-nav">' . $twentytwentyone_prev . $twentytwentyone_previous_label . '</p><p class="post-title">%title</p>',
-		)
-	);
 endwhile; // End of the loop.
 
 get_footer();
